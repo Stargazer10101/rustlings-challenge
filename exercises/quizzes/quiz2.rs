a0538c8479a53a777acd12344276fde0790a36ce
Stargazer1010 <nitishitankar9102@gmail.com>
2024-12-02 17:22:08 +0530

    Done till hashmaps. Quiz 2 remaining

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,12 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+     pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+
+        
+
+
+     }
 }
 
 fn main() {
